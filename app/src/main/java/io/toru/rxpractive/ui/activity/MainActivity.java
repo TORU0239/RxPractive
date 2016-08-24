6a44ea5f0cf26edf3c1782391463f048e9083e3b
Toru Choi <toru@zikto.com>
2016-08-24 18:56:07 +0900

    license dialog update

--- app/src/main/java/io/toru/rxpractive/ui/activity/MainActivity.java
+++ app/src/main/java/io/toru/rxpractive/ui/activity/MainActivity.java
@@ -119,21 +119,51 @@ public class MainActivity extends BaseActivity implements MainView {
         switch (item.getItemId()){
             case R.id.menu_setting:
 
-                final String name = "LicensesDialog";
-                final String url = "http://psdev.de";
-                final String copyright = "Copyright 2013 Philip Schiffer <admin@psdev.de>";
-                final License license = new ApacheSoftwareLicense20();
-                final Notice notice = new Notice(name, url, copyright, license);
-                
+                final String rxAndroid = "RxAndroid";
+                final String rxAndroidurl = "https://github.com/ReactiveX/RxAndroid";
+                final String rxAndroidcopyright = "Copyright 2015 The RxAndroid authors";
+                final License rxAndroidlicense = new ApacheSoftwareLicense20();
+                final Notice rxJavanotice = new Notice(rxAndroid, rxAndroidurl, rxAndroidcopyright, rxAndroidlicense);
+
+                final String retrofit = "Retrofit";
+                final String retrofiturl = " http://square.github.io/retrofit/";
+                final String retrofitcopyright = "Copyright 2013 Square, Inc";
+                final License retrofitlicense = new ApacheSoftwareLicense20();
+                final Notice retrofitnotice = new Notice(retrofit, retrofiturl, retrofitcopyright, retrofitlicense);
+
+                final String okhttp = "Retrofit";
+                final String okhttpurl = " http://square.github.io/okhttp/";
+                final String okhttpcopyright = "Copyright 2013 Square, Inc";
+                final License okhttplicense = new ApacheSoftwareLicense20();
+                final Notice okhttpnotice = new Notice(okhttp, okhttpurl, okhttpcopyright, okhttplicense);
+
                 final String glide = "Glide";
                 final String glideurl = "https://github.com/bumptech/glide";
                 final String glidecopyright = "Copyright 2014 Google, Inc. ";
                 final License glidelicense = new BSD2ClauseLicense();
                 final Notice glidenotice = new Notice(glide, glideurl, glidecopyright, glidelicense);
 
+                final String btKnife = "ButterKnife";
+                final String btKnifeurl = "http://jakewharton.github.io/butterknife/";
+                final String btKnifecopyright = "Copyright 2013 Jake Wharton";
+                final License btKnifelicense = new ApacheSoftwareLicense20();
+                final Notice btKnifenotice = new Notice(btKnife, btKnifeurl, btKnifecopyright, btKnifelicense);
+
+                final String name = "LicensesDialog";
+                final String url = "http://psdev.de";
+                final String copyright = "Copyright 2013 Philip Schiffer <admin@psdev.de>";
+                final License license = new ApacheSoftwareLicense20();
+                final Notice notice = new Notice(name, url, copyright, license);
+
                 Notices notices = new Notices();
-                notices.addNotice(notice);
+
+                notices.addNotice(rxJavanotice);
+                notices.addNotice(retrofitnotice);
+                notices.addNotice(btKnifenotice);
+                notices.addNotice(okhttpnotice);
                 notices.addNotice(glidenotice);
+                notices.addNotice(btKnifenotice);
+                notices.addNotice(notice);
 
                 new LicensesDialog.Builder(this)
                         .setNotices(notices)
