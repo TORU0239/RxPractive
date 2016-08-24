bec4ff7d9f36f146454ab8b3df27a97013895e72
Toru Choi <toru@zikto.com>
2016-08-24 17:55:58 +0900

    toolbar menu and license dialog added

--- app/src/main/java/io/toru/rxpractive/ui/activity/MainActivity.java
+++ app/src/main/java/io/toru/rxpractive/ui/activity/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -17,6 +20,12 @@ import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.BSD2ClauseLicense;
+import de.psdev.licensesdialog.licenses.License;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 import io.toru.rxpractive.R;
 import io.toru.rxpractive.base.activity.BaseActivity;
 import io.toru.rxpractive.pattern.model.WeatherForecast;
@@ -97,5 +106,42 @@ public class MainActivity extends BaseActivity implements MainView {
     @Override
     public void onLoadingFinish() {
         progressBar.dismiss();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_setting:
+
+                final String name = "LicensesDialog";
+                final String url = "http://psdev.de";
+                final String copyright = "Copyright 2013 Philip Schiffer <admin@psdev.de>";
+                final License license = new ApacheSoftwareLicense20();
+                final Notice notice = new Notice(name, url, copyright, license);
+                
+                final String glide = "Glide";
+                final String glideurl = "https://github.com/bumptech/glide";
+                final String glidecopyright = "Copyright 2014 Google, Inc. ";
+                final License glidelicense = new BSD2ClauseLicense();
+                final Notice glidenotice = new Notice(glide, glideurl, glidecopyright, glidelicense);
+
+                Notices notices = new Notices();
+                notices.addNotice(notice);
+                notices.addNotice(glidenotice);
+
+                new LicensesDialog.Builder(this)
+                        .setNotices(notices)
+                        .build()
+                        .showAppCompat();
+
+                break;
+        }
+        return true;
     }
 }
